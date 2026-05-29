8d68f5fb5763113d3304802550606fc72c673661
Anley-dev <anleybelay72@gmail.com>
2026-05-29 08:46:47 +0300

    Add Firebase config for GitHub Pages

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - Firebase configuration for GitHub Pages
+// This file contains the Firebase config that will be used in production
+window.__FIREBASE_CONFIG__ = {
+    apiKey: "AIzaSyDSlXqHHVNYcx8WTb66RvJazDaMCl3l6B8",
+    authDomain: "imcs-wachamo.firebaseapp.com",
+    projectId: "imcs-wachamo",
+    storageBucket: "imcs-wachamo.firebasestorage.app",
+    messagingSenderId: "445125483030",
+    appId: "1:445125483030:web:2eaa94ecac7a13ef0fb337"
+};
